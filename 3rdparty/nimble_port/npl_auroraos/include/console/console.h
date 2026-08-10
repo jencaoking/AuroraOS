0f02bdf3e6824904dc282cf1d9d55e59939a7713
jencaoking <663930657@qq.com>
2026-08-10 20:16:24 +0800

    feat: 添加文件下载完成后的打开目录功能

---
+++ 3rdparty/nimble_port/npl_auroraos/include/console/console.h
@@ -0,0 +1,15 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Stub for NimBLE's console inclusion
+#define console_printf(...)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CONSOLE_H
