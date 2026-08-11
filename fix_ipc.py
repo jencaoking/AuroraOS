04e57440bdee7552200d6b0eb37124daa6539c9c
jencaoking <663930657@qq.com>
2026-08-11 19:51:26 +0800

    chore(nimble): 更新 nimble 子模块引用

--- fix_ipc.py
+++
@@ -1,9 +0,0 @@
-import os, re
-p = 'kernel/core/ipc.hpp'
-with open(p, 'rb') as f: data = f.read()
-try: text = data.decode('gbk')
-except: text = data.decode('utf-16le', errors='replace')
-text = text.replace('#include "kernel_object.hpp"', '#include "kernel_object.hpp"\n#include "../task/wait_queue.hpp"')
-text = text.replace(', send_head_(nullptr), send_tail_(nullptr), recv_head_(nullptr), recv_tail_(nullptr)', '')
-text = re.sub(r'private:\s*void enqueue_sender.*?recv_tail_;', 'private:\n    WaitQueue send_queue_;\n    WaitQueue recv_queue_;', text, flags=re.DOTALL)
-with open(p, 'w', encoding='utf-8') as f: f.write(text)
